4c00eee36494d10a047855de7575b2426cc03f6f
Burak <burak@bla.com>
2016-05-09 12:30:45 +0300

    -

--- foam/futures/fut2.py
+++ foam/futures/fut2.py
@@ -2,15 +2,24 @@
 # N - Jul, Q - Aug, U - Sep, V - Oct, W - Nov, Z - Dec
 #
 import Quandl, os, itertools, sys, logging
+sys.path.append("..")
 import pandas as pd
+from memo import *
 
-def download_data():
-
+@memo                                    
+def get_quandl_auth():
     fname = '%s/.quandl' % os.environ['HOME']
     if not os.path.isfile(fname):
         print 'Please create a %s file ' % fname
         exit()
     auth = open(fname).read()
+    return auth
+
+def web_download(contract):
+    df = Quandl.get(contract, returns="pandas",authtoken=get_quandl_auth())
+    return df
+
+def download_data(downloader=web_download):
 
     years = range(1984,2022)
     months = ['F', 'G', 'H', 'J', 'K', 'M',
@@ -24,7 +33,7 @@ def download_data():
                 contract = "%s/%s%s%d" % (market,sym,month,year)
                 try:
                     print contract
-                    df = Quandl.get(contract, returns="pandas",authtoken=auth)
+                    df = web_download(contract)
                     print df.head()
                     print df.columns
                 except Quandl.Quandl.DatasetNotFound:
