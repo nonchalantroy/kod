480aacfef2bdaecaf70fa65c68d1ec835b7d6df9
Burak <burak@bla.com>
2016-05-22 20:01:38 +0300

    -

--- site-lisp/peval.py
+++ site-lisp/peval.py
@@ -8,6 +8,7 @@ import re, random
 from turkish.deasciifier import Deasciifier
 
 interactions = {}
+glob = {}
 
 def get_block_content(start_tag, end_tag):
     remember_where = lisp.point()
@@ -19,14 +20,22 @@ def get_block_content(start_tag, end_tag):
     return block_begin, block_end, content
 
 def pexec():
+    global glob
     remember_where = lisp.point()
     block_begin, block_end, content = get_block_content("\n","\n")
-    res = eval(content)
-    lisp.forward_line(2)
-    bb1, be2, bc2 = get_block_content("\n","\n")
-    lisp.delete_region(bb1,be2)
-    lisp.insert("\n")
-    lisp.insert(str(res))
+    if "=" in content or "import" in content:
+        c = compile(source=content,filename="",mode="single")
+        eval(c,glob)
+        print 'eval done'
+    else:
+        c = compile(source=content,filename="",mode="eval")
+        res = eval(c,glob)
+        lisp.forward_line(2)
+        bb1, be2, bc2 = get_block_content("\n","\n")
+        lisp.delete_region(bb1,be2)
+        lisp.insert("\n")
+        lisp.insert(str(res))
     lisp.goto_char(remember_where)
             
 interactions[pexec] = ''
+
