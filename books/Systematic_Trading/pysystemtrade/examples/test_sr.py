84dbfcac1d0b14ac1fb2e0d2e4d12f79e3ac4b6c
Burak <burak@bla.com>
2016-06-17 08:53:00 +0300

    -

--- books/Systematic_Trading/pysystemtrade/examples/test_sr.py
+++ books/Systematic_Trading/pysystemtrade/examples/test_sr.py
@@ -7,7 +7,20 @@ from syscore.dateutils import MONTHS_IN_YEAR, ROOT_MONTHS_IN_YEAR
 DEFAULT_CAPITAL = 1.0
 DEFAULT_ANN_RISK_TARGET = 0.16
 
-def sharpe(price, forecast):        
+def sharpe(price, forecast):
+    """
+    Herein the proof why this position calculation is correct (see chapters
+    5-11 of 'systematic trading' book)
+    Position = forecast x instrument weight x instrument_div_mult x vol_scalar / 10.0
+             = forecast x instrument weight x instrument_div_mult x daily cash vol target / (10.0 x instr value volatility)
+             = forecast x instrument weight x instrument_div_mult x daily cash vol target / (10.0 x instr ccy volatility x fx rate)
+             = forecast x instrument weight x instrument_div_mult x daily cash vol target / (10.0 x block value x % price volatility x fx rate)
+             = forecast x instrument weight x instrument_div_mult x daily cash vol target / (10.0 x underlying price x 0.01 x value of price move x 100 x price change volatility/(underlying price) x fx rate)
+             = forecast x instrument weight x instrument_div_mult x daily cash vol target / (10.0 x value of price move x price change volatility x fx rate)
+    Making some arbitrary assumptions (one instrument, 100% of capital, daily target DAILY_CAPITAL):
+             = forecast x 1.0 x 1.0 x DAILY_CAPITAL / (10.0 x value of price move x price diff volatility x fx rate)
+             = forecast x  multiplier / (value of price move x price change volatility x fx rate)
+    """        
     base_capital = DEFAULT_CAPITAL
     daily_risk_capital = DEFAULT_CAPITAL * DEFAULT_ANN_RISK_TARGET / ROOT_BDAYS_INYEAR        
     ts_capital=pd.Series([DEFAULT_CAPITAL]*len(price), index=price.index)        
@@ -20,8 +33,7 @@ def sharpe(price, forecast):
     cum_trades = positions.shift(1).ffill()
     trades_to_use=cum_trades.diff()
     price_returns = price.diff()
-    instr_ccy_returns = cum_trades.shift(1)* price_returns 
-    #instr_ccy_returns = cum_trades * price_returns 
+    instr_ccy_returns = cum_trades.shift(1)*price_returns 
     instr_ccy_returns=instr_ccy_returns.cumsum().ffill().reindex(price.index).diff()
     base_ccy_returns = instr_ccy_returns 
     mean_return = base_ccy_returns.mean() * BUSINESS_DAYS_IN_YEAR
