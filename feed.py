582d5dcecf895f30e7457731f4e2d3efeb73aa94
Burak <burak@bla.com>
2016-07-12 16:22:36 +0300

    -

--- feed.py
+++ feed.py
@@ -3,19 +3,17 @@ import feedparser
 
 def get_feeds():
 
-    feeds = ['http://feeds.reuters.com/reuters/topNews',
-             'http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml',
-             'http://www.huffingtonpost.com/feeds/verticals/world/index.xml'
+     feeds = [("Reuters",'http://feeds.reuters.com/reuters/topNews'),
+              ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
+              ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml')
+              
     ]
 
-    for feed in feeds:
-        d = feedparser.parse(feed)
-        print "==", feed
+     for feed in feeds:
+        print "\n#", feed[0], "\n"
+        d = feedparser.parse(feed[1])
         for post in d.entries:
             if ".mp3" in post.link: continue
-            p = post.title
-            p = p.replace("Bloomberg Advantage: ","")
-            p = p.replace("The Bloomberg Advantage: ","")
-            print "[[%s][%s]]" % (post.link, p.decode("utf-8"))
+            print "[[%s][%s]]" % (post.link, post.title)
 
 get_feeds()
