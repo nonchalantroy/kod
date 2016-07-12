6e24427c510bcca52e719ab8e15c7e84bf0ce06e
Burak <burak@bla.com>
2016-07-12 12:03:02 +0300

    -

--- time.py
+++ time.py
@@ -4,13 +4,17 @@
 # the credentials to get a real Internet connection, I can still get
 # the time information from that server, through Wifi.
 #
-# Usage: time.py [IP ADDRESS]
+# Usage: time.py [URL ADDRESS]
 #
-# Ip address could be the Default Gateway address (see the output of ifconfig
-# or ipconfig, or route print, or nslookup. 
+# Examples: time.py https://10.10.1.1 or time.py http://192.168.10.1:5001
+#
+# Ip address of the web server could be the Default Gateway address
+# -see the output of ifconfig or ipconfig, or route print, or nslookup-
+# or simply be retrieved from your browser by copy and paste.
 #
 import urllib2, sys, re
-req = urllib2.Request('http://%s' % sys.argv[1])
+
+req = urllib2.Request(sys.argv[1])
 res = urllib2.urlopen(req)
 res.close()
 content = str(res.info())
