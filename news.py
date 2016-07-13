660ce4fac18ebe7b114ff2cdc405830c4200f62b
Burak <burak@bla.com>
2016-07-13 12:02:57 +0300

    -

--- news.py
+++ news.py
@@ -3,7 +3,10 @@ import feedparser
 
 def get_feeds():
 
-     feeds = [("Reuters",'http://feeds.reuters.com/reuters/topNews'),
+     feeds = [("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews'),
+              ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews'),
+              ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
+              ("Reuters (Economy)", "http://feeds.reuters.com/news/economy"),
               ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
               ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml')
               
