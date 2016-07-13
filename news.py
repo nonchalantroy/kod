19d09a5b0551f1d79257fd1722847fc255fdba89
Burak <burak@bla.com>
2016-07-13 12:36:37 +0300

    -

--- news.py
+++ news.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from urllib import FancyURLopener
 import feedparser, sys, codecs
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
@@ -12,10 +11,10 @@ def get_feeds():
               ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
               ("Reuters (Economy)", "http://feeds.reuters.com/news/economy"),
               ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
-              ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml')              
+              ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml'),
+              ("The Guardian","http://www.theguardian.com/world/rss")
     ]
 
-                   
      sys.stdout = codecs.getwriter('utf8')(sys.stdout)
      sys.stderr = codecs.getwriter('utf8')(sys.stderr)
      
@@ -23,6 +22,6 @@ def get_feeds():
         print "\n#", feed[0], "\n"
         d = feedparser.parse(feed[1])
         for post in d.entries:
-            print "[[%s][%s]]" % (post.link,post.title)
+            print "[[%s][%s]]" % (post.link,repr(post.title))
 
 get_feeds()
