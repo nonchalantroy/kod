b990d7140efe078370c00c40a1eb21021226e7c8
Burak <burak@bla.com>
2016-07-18 11:28:23 +0300

    -

--- news.py
+++ news.py
@@ -2,13 +2,15 @@
 import feedparser, sys, codecs
 import re, time, os
 
-feeds = [("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews'),
-         ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews'),
-         ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
-         ("Reuters (Economy)", "http://feeds.reuters.com/news/economy"),
-         ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
-         ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml'),
-         ("The Guardian","http://www.theguardian.com/world/rss")
+feeds = [
+    ("Hurriyet", "http://www.hurriyet.com.tr/rss/gundem"),
+    ("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews'),
+    ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews'),
+    ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
+    ("Reuters (Economy)", "http://feeds.reuters.com/news/economy"),
+    ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
+    ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml'),
+    ("The Guardian","http://www.theguardian.com/world/rss")
 ]
 
      
@@ -22,6 +24,8 @@ def show():
         print("\n")
         d = feedparser.parse(feed[1])
         for post in d.entries:
-            print("[[%s][%s]]" % (post.link,repr(post.title)))
+            if len(re.findall(r"Erdo.an", repr(post.title))) > 0: continue
+            print("[[%s][%s]]" % (post.link, post.title))
+
 
 show()            
