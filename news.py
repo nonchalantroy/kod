bfcd200c729547908607308757b223e5d6a9acdc
Burak <burak@bla.com>
2016-07-19 09:24:00 +0300

    -

--- news.py
+++ news.py
@@ -5,27 +5,26 @@ Python replacement for newsbeuter, an RSS based news reader.
 import feedparser, sys, codecs
 import re, time, os
 
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
 def show():
 
-    feeds = [
-        ("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews'),
-        ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews'),
-        ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
-        ("Reuters (Economy)", "http://feeds.reuters.com/news/economy"),
-        ("Bloomberg","https://twitrss.me/twitter_user_to_rss/?user=business"),
-        ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
-        ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml'),
-        ("The Guardian","http://www.theguardian.com/world/rss"),
-        ("Cumhuriyet","http://www.cumhuriyet.com.tr/rss/son_dakika.xml"),
-        ("Hurriyet", "http://www.hurriyet.com.tr/rss/gundem"),
-        ("Al-Jazeera","http://aljazeera.com.tr/rss.xml"),
-        ("Acik Gazete","https://www.acikgazete.com/feed/"),
-        ("Diken","http://www.diken.com.tr/feed/"),
-        ("T24","https://twitrss.me/twitter_user_to_rss/?user=t24comtr")
+    feeds = [("The Guardian","http://www.theguardian.com/world/rss"),
+             ("Diken","http://www.diken.com.tr/feed/"),
+             ("Cumhuriyet","http://www.cumhuriyet.com.tr/rss/son_dakika.xml"),
+             ("Hurriyet", "http://www.hurriyet.com.tr/rss/gundem"),
+             ("Al-Jazeera","http://aljazeera.com.tr/rss.xml"),
+             ("Acik Gazete","https://www.acikgazete.com/feed/"),
+             ("T24","https://twitrss.me/twitter_user_to_rss/?user=t24comtr"),
+             ("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews'),
+             ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews'),
+             ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
+             ("Reuters (Economy)", "http://feeds.reuters.com/news/economy"),
+             ("Bloomberg","https://twitrss.me/twitter_user_to_rss/?user=business"),
+             ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml'),
+             ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml')
     ]
-    
-    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-    sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
     if len(sys.argv) == 2 and sys.argv[1] == "x":
         feeds = [("Fuat Avni","https://twitrss.me/twitter_user_to_rss/?user=fuatavni_f")]
