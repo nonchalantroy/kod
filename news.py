6a5547fb1e351fff6b143c5a9ca9c80d2c8db5a6
Burak <burak@bla.com>
2016-07-22 10:03:25 +0300

    -

--- news.py
+++ news.py
@@ -10,34 +10,34 @@ sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 def show():
 
-    feeds = [("The Guardian","http://www.theguardian.com/world/rss"),
-             ("Diken","http://www.diken.com.tr/feed/"),
-             ("Cumhuriyet","http://www.cumhuriyet.com.tr/rss/son_dakika.xml"),
-             (u"Hürriyet", "http://www.hurriyet.com.tr/rss/gundem"),
-             ("Al-Jazeera","http://aljazeera.com.tr/rss.xml"),
-             (u"Açık Gazete","https://www.acikgazete.com/feed/"),
-             ("T24","https://twitrss.me/twitter_user_to_rss/?user=t24comtr"),
-             ("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews'),
-             ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews'),
-             ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews"),
-             ("Bloomberg","https://twitrss.me/twitter_user_to_rss/?user=business"),
-             ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml'),
-             ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml'),
-             ("Sputnik News","http://tr.sputniknews.com/export/rss2/archive/index.xml"),
-             ("EB","https://twitrss.me/twitter_user_to_rss/?user=ebabahan"),
-             ("Fuat Avni","https://twitrss.me/twitter_user_to_rss/?user=fuatavni_f")
+    feeds = [("The Guardian","http://www.theguardian.com/world/rss",10),
+             ("Diken","http://www.diken.com.tr/feed/",-1),
+             ("Cumhuriyet","http://www.cumhuriyet.com.tr/rss/son_dakika.xml",10),
+             (u"Hürriyet", "http://www.hurriyet.com.tr/rss/gundem",10),
+             ("Al-Jazeera","http://aljazeera.com.tr/rss.xml",-1),
+             (u"Açık Gazete","https://www.acikgazete.com/feed",-1),
+             ("T24","https://twitrss.me/twitter_user_to_rss/?user=t24comtr",-1),
+             ("Reuters (Top News)",'http://feeds.reuters.com/reuters/topNews',-1),
+             ("Reuters (UK World)",'http://feeds.reuters.com/reuters/UKWorldNews',-1),
+             ("Reuters (World)",'http://feeds.reuters.com/reuters/worldNews',-1),
+             ("Reuters (Business)", "http://feeds.reuters.com/reuters/businessNews",-1),
+             ("Bloomberg","https://twitrss.me/twitter_user_to_rss/?user=business",-1),
+             ('Huffington Post','http://www.huffingtonpost.com/feeds/verticals/world/index.xml',-1),
+             ('BBC','http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml',20),
+             ("Sputnik News","http://tr.sputniknews.com/export/rss2/archive/index.xml",15),
+             ("EB","https://twitrss.me/twitter_user_to_rss/?user=ebabahan",-1),
+             ("Fuat Avni","https://twitrss.me/twitter_user_to_rss/?user=fuatavni_f",-1)
     ]
 
-    for feed in feeds:
+    for name,url,lim in feeds:
         print("\n")
-        print("## " + feed[0])
+        print("## " + name)
         print("\n")
-        d = feedparser.parse(feed[1])
-        for post in d.entries:
+        d = feedparser.parse(url)
+        for i,post in enumerate(d.entries):
             link = post.link; title = post.title
             if len(re.findall(r"Erdo.an", title)) > 0: continue
-            #print("[[%s][%s]]" % (link,title))
             print("[[%s][%s]]" % (link,unicode(title)))
-
+            if lim > 0 and i==lim: break
 
 show()            
