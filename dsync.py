930d5520002f4c3ad1a8dd79f4cd97c0af16072f
Burak <burak@bla.com>
2016-08-02 12:19:36 +0300

    -

--- dsync.py
+++ dsync.py
@@ -3,17 +3,17 @@ import os, sys, shutil, rsync, re
 if len(sys.argv) < 2: print "Usage dsync.py [letter]"; exit()
 
 if sys.argv[1] == "d":
+    os.system("python rsync.py c:\\Users\\burak\\Documents\\quant_at e:\\archive\\quant_at --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\Dropbox e:\\archive\\Dropbox --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\classnotes e:\\archive\\classnotes --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\kod e:\\archive\\kod --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\book_idx e:\\archive\\book_idx --delete")
-    os.system("python rsync.py c:\\Users\\burak\\Documents\\quant_at e:\\archive\\quant_at --delete")
 
 if sys.argv[1] == "e":
+    os.system("python rsync.py c:\\Users\\burak\\Documents\\quant_at d:\\quant_at --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\classnotes d:\\classnotes --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\kod d:\\kod --delete")
     os.system("python rsync.py c:\\Users\\burak\\Documents\\book_idx d:\\book_idx --delete")
-    os.system("python rsync.py c:\\Users\\burak\\Documents\\quant_at d:\\quant_at --delete")
     
 if sys.argv[1] == "ed":
     os.system("python rsync.py E:\\kitaplar D:\\archive\\kitaplar")
