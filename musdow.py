876fa61ad1a49ab9f1cab9a1c6eff3d242e75e45
Burak <burak@bla.com>
2016-09-29 14:07:31 +0300

    -

--- musdow.py
+++ musdow.py
@@ -1,14 +1,15 @@
 '''
-Downloads 10 songs from a known site.
+Downloads 10 songs from a few known sites.
 '''
 import re, requests, random, os
 url = "http://www.sos-jukebox.net/music"
+#url = "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/_jazz_"
 response = requests.get(url)
 response_body = response.content
-regex = "<li><a href=\"(.*?)\">.*?</a></li>"
+regex = "a href=\"(.*?mp3)\">.*?</a>"
 songs = re.findall(regex, response_body, re.DOTALL)
 os.chdir("c:/Users/burak/Music")
-for i in range(10):
+for i in range(1):
     idx = random.choice(range(len(songs)))
     if ".mp3" in songs[idx]: os.system("wget %s/%s" % (url,songs[idx]))
    
