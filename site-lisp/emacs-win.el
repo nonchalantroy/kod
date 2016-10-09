3afcfbf48582e2d0a0efb68c7f25e873cc240972
Burak <burak@bla.com>
2016-10-09 22:43:09 +0300

    -

--- site-lisp/emacs-win.el
+++ site-lisp/emacs-win.el
@@ -912,10 +912,7 @@ This command does not push erased text to kill-ring."
 (find-file-other-window "c:/Users/burak/Documents")
 (find-file-other-window "c:/Users/burak/Documents/kod")
 (find-file-other-window "c:/Users/burak/Downloads")
-(find-file-other-window "c:/Users/burak/Documents/kod/books/Systematic_Trading/pysystemtrade")
 (find-file-other-window "c:/Users/burak/Documents/quant_at/data")
-(find-file-other-window "c:/Users/burak/Documents/quant_at/alg/priv")
-(find-file-other-window "c:/Users/burak/Documents/Dropbox/resmi/newbusiness")
 
 (switch-to-buffer "*scratch*")
 (delete-other-windows)
